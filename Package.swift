7f007e4d92665aec1e66f22abd5c42e83bb63fc3
杨俊艺 <1209032479@qq.com>
2026-06-03 16:58:06 +0800

    1.添加依赖

--- Package.swift
+++ Package.swift
@@ -6,22 +6,21 @@ import PackageDescription
 let package = Package(
     name: "DevelopKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DevelopKit",
-            targets: ["DevelopKit"]
-        ),
+        .library(name: "DevelopKit", targets: ["DevelopKit"]),
     ],
+	dependencies: [
+		.package(url: "https://github.com/devxoul/Then", branch: "main"),
+		.package(url: "https://github.com/ReactiveX/RxSwift", exact: "6.9.1"),
+		.package(url: "https://github.com/SnapKit/SnapKit", exact: "5.7.1"),
+		.package(url: "https://github.com/nicklockwood/SwiftFormat", branch: "main"),
+	],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DevelopKit"
+        .target(name: "DevelopKit",
+				dependencies: [
+					"Then",
+				]
         ),
-        .testTarget(
-            name: "DevelopKitTests",
-            dependencies: ["DevelopKit"]
-        ),
+        .testTarget(name: "DevelopKitTests", dependencies: ["DevelopKit"]),
     ],
     swiftLanguageModes: [.v6]
 )
